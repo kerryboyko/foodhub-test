88c7d629af6c1475f8424880a1eb9e6c76d71ce9
kerryboyko <kerry.boyko@gmail.com>
2026-06-09 13:56:03 +0100

    Style: Format

--- src/app/hello/hello.spec.ts
+++ src/app/hello/hello.spec.ts
@@ -1,9 +1,9 @@
-import { describe, it, expect } from "vitest";
-import { helloWorld } from "./hello";
+import { describe, it, expect } from 'vitest';
+import { helloWorld } from './hello';
 
-describe("helloWorld", () => {
-  it("should return a greeting message", () => {
-    const result = helloWorld("World");
-    expect(result).toBe("Hello World!");
+describe('helloWorld', () => {
+  it('should return a greeting message', () => {
+    const result = helloWorld('World');
+    expect(result).toBe('Hello World!');
   });
 });
