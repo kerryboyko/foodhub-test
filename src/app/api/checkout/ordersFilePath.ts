4f20e4008ca131b57fd7fa16e6c363dbb7f2fdcf
kerryboyko <kerry.boyko@gmail.com>
2026-06-11 01:15:45 +0100

    Issue with performance

--- src/app/api/checkout/ordersFilePath.ts
+++ src/app/api/checkout/ordersFilePath.ts
@@ -1,3 +1,3 @@
 import path from 'path';
 
-export const ordersFilePath = path.join(process.cwd(), 'data', 'orders.json');
+export const ordersFilePath = path.join(process.cwd(), 'output', 'orders.json');
