c4f64bd47bdbd13448e112fc47edf793646e072f
kerryboyko <kerry.boyko@gmail.com>
2026-06-13 23:25:35 +0100

    Hotfix for playwright misconfiguration

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  testMatch: '**/*.e2e.ts',
   use: {
     baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry'
